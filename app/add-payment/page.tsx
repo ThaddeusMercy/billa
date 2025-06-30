f52fb008de063e476f2c1ad3efcd597410332936
Mercy Thaddeus <mercythaddeus234+github@gmail.com>
2025-06-30 06:02:14 +0100

    Updated page.tsx

--- app/add-payment/page.tsx
+++ app/add-payment/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+export const dynamic = 'force-dynamic'
+
 import { useState, useRef, useCallback, useEffect, Suspense } from "react"
 import Link from "next/link"
 import { useRouter, useSearchParams } from "next/navigation"
