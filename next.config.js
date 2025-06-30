e3b30eee03859696dc258545133d123dc460c527
Mercy Thaddeus <mercythaddeus234+github@gmail.com>
2025-06-30 05:25:51 +0100

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-  experimental: {
-    dynamicIO: false,
-  },
   webpack: (config) => {
     config.ignoreWarnings = [
       {
